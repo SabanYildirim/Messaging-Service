fcb41543be5d9a446e8cc4606b5377bbb68a3514
şaban yıldırım <saban.yildirim@nesine.com>
2021-09-16 21:37:16 +0300

    MessagingService create project

---
+++ src/MessagingServiceApp.Api/Controllers/v1/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessagingServiceApp.Api.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("/getall")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("");
+        }
+    }
+}
